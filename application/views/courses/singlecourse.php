5d806e6e97b2ded7572fce350a41507f2b1ff119
KathyDimitrova <kathy.dimitrova@gmail.com>
2015-11-07 11:43:53 +0200

    singlecourse

--- application/views/courses/singlecourse.php
+++ application/views/courses/singlecourse.php
@@ -1,5 +1,28 @@
-<div class="singlecourse"
-     h1{ background-color: #6495ed; }
- >
- <h1>This is the about page</h1>
+<style>
+div.title {
+    background-color:salmon;
+    color:black;
+    border-bottom:1px solid black;
+    margin:20px;
+    padding:20px;
+}	
+div.course {
+    background-color:#FFA07A;
+    color:black;
+    border: 1px solid black;
+    margin:20px;
+    padding:20px;
+}
+
+</style>
+<body>
+    <div class="title">
+        <h1>Title single course</h1>
+    </div>
+    <div class="course">
+        Name course:
+        Lector:
+        Start Date: 
+    </div>
     
+</body>
