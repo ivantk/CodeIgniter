9715e27d86b56f844870e1474bda08f62b6be7c2
bachtech <ba4_te4o@abv.bg>
2015-11-07 12:53:33 +0200

    -

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,8 +1,17 @@
-
 <html>
         <head>
-                <title>CodeIgniter Tutorial</title>
+            <meta http-equiv="content-type" content="text/html" charset="UTF-8"/>
+            <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/header_style.css" />
+                <title>BIG MAZALQK</title>
         </head>
         <body>
+          <div id="header">
+<img src="<?php echo base_url(); ?>img/LOGOO.png" id="logo" />
+<h1>you are now in section <?php echo $title; ?></h1> 
+<div/>
+<br style="clear" />
+             
+  
 
-                <h1><?php echo $title; ?></h1>
+    
+    
